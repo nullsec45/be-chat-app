489b8f93d560d8d6a5da7319d48ead92e7e9a28a
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2026-01-26 23:02:38 +0700

    feat : add cooki parser in main file.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.ts";
 import { connectDB } from "./lib/database.ts";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const PORT=process.env.APP_PORT;
 
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
