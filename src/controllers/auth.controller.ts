1154d46f9a1c62c3a3bd174bb789c66b2f9205f1
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2026-01-26 22:47:19 +0700

    feat(auth)   : update profile & my profile.

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,8 @@
 import type { Request, Response } from 'express';
 import User from '../models/user.model.ts';
 import bcrypt from 'bcryptjs';
-import {generateToken} from "../lib/utils.ts";
+import { generateToken } from "../lib/utils.ts";
+import cloudinary from "../lib/cloudinary.ts";
 
 export const signup=async (req:Request,res:Response) => {
    const { name, email, password }=req.body;
@@ -48,18 +49,84 @@ export const signup=async (req:Request,res:Response) => {
    }
 }
 
-export const signin=(req:Request, res:Response) => {
-   try{
-    
-   }catch(error){
+export const signin=async (req:Request, res:Response) => {
+   const { email, password } = req.body;
 
+   try{
+      const user=await User.findOne({email});
+
+      if (!user) {
+         return res.status(400).json({message:"Invalid credentials"});
+      }
+
+      const isPasswordCorrect=await bcrypt.compare(password, user.password);
+
+      if(!isPasswordCorrect) {
+         return res.status(400).json({message:"Invalid credentials"});
+      }
+
+      generateToken(user._id.toString(), res);
+
+      res.status(200).json({
+         _id:user._id,
+         fullName:user.fullName,
+         email:user.email,
+         profilePict:user.profilePict
+      })
+   }catch(error:any){
+      console.log("Error in login controller", error.message);
+      res.status(500).json({message:"Internal Server Error"});
    }
 }
 
 export const logout=(req:Request, res:Response) => {
    try{
-    
-   }catch(error){
-
+      res.cookie("jwt","", {maxAge:0});
+      res.status(200).json({message:"Logged out successfully"});
+   }catch(error:any){
+      console.log("Error in logout controller", error.message);
+      res.status(500).json( {message:"Internal Server Error"})
    }
 }
+
+
+export const updateProfile=async (req:Request, res:Response) => {
+   try{
+      const { profilePict="", name, email } = req.body;
+      const userId=req.user._id;
+
+      // if(!profilePict) {
+      //    return res.status(400).json({message:"Profile Pict is Required"});
+      // }
+
+      let uploadResponse;
+      if (profilePict){
+         uploadResponse=await cloudinary.uploader.upload(profilePict);
+      }
+
+      const updatedUser=await User.findByIdAndUpdate(
+         userId,
+         { 
+            profilePict:uploadResponse?.secure_url,
+            fullName:name,
+            email, 
+         },
+         { new:true }
+      );
+
+      res.status(200).json(updatedUser);
+   }catch(error:any){
+      console.log("Error in update profile controller", error.message);
+      res.status(500).json( {message:"Internal Server Error"});
+   }
+}
+
+
+export const myProfile=async(req:Request, res:Response) => {
+   try{
+       res.status(200).json(req.user);
+   }catch(error:any){
+      console.log("Error in my profile controller", error.message);
+      res.status(500).json( {message:"Internal Server Error"});
+   }
+} 
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,14 +1,18 @@
 import express from "express";
-import { signin, signup, logout } from "../controllers/auth.controller.ts";
+import { signin, signup, logout, updateProfile, myProfile } from "../controllers/auth.controller.ts";
+import { protectRoute } from "../middleware/auth.middleware.ts";
 
 const authRoutes=express.Router();
 
-authRoutes.get("/signin",signin);
+authRoutes.post("/signin",signin);
 
 authRoutes.post("/signup", signup);
 
 authRoutes.post("/logout", logout);
 
+authRoutes.put("/update-profile", protectRoute, updateProfile);
+
+authRoutes.get("/my-profile", protectRoute, myProfile);
 
 
 export default authRoutes;
