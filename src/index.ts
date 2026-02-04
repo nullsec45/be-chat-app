3a6e610df11741e4e5ab25cb9f5536d4419f3270
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2026-02-04 23:22:50 +0700

    feat : add cors.

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.route.ts";
 import { connectDB } from "./lib/database.ts";
 import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/message.route.ts";
+import cors from "cors";
 
 dotenv.config();
 
@@ -13,6 +14,11 @@ const PORT=process.env.APP_PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+}))
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
